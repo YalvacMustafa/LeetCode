3bceac646658d99e3d719fcf297599398721c0aa
YalvacMustafa <zapza574@gmail.com>
2023-08-01 14:16:46 +0300

    Ekleme

---
+++ Example13/Main.java
@@ -0,0 +1,59 @@
+package leetcode.Example13;
+
+/**
+ * Input: mat = [[1,2,3],
+ * [4,5,6],
+ * [7,8,9]]
+ * Output: 25
+ * Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+ * Notice that element mat[1][1] = 5 is counted only once.
+ * 
+ * Example 2:
+ * 
+ * Input: mat = [[1,1,1,1],
+ * [1,1,1,1],
+ * [1,1,1,1],
+ * [1,1,1,1]]
+ * Output: 8
+ */
+ class MatrixDiagonalSum {
+    public int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += mat[i][i]; // Adding elements from the primary diagonal
+            sum += mat[i][n - 1 - i]; // Adding elements from the secondary diagonal
+        }
+
+        // If the matrix size is odd, one element from the secondary diagonal will be counted twice,
+        // so we need to subtract it.
+        if (n % 2 == 1) {
+            sum -= mat[n / 2][n / 2];
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        MatrixDiagonalSum diagonalSumCalculator = new MatrixDiagonalSum();
+        
+        int[][] mat1 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        System.out.println(diagonalSumCalculator.diagonalSum(mat1)); // Output: 25
+
+        int[][] mat2 = {
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1},
+            {1, 1, 1, 1}
+        };
+        System.out.println(diagonalSumCalculator.diagonalSum(mat2)); // Output: 8
+
+        int[][] mat3 = {{5}};
+        System.out.println(diagonalSumCalculator.diagonalSum(mat3)); // Output: 5
+    }
+}
